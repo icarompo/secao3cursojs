6c820bff03c54ee22674e947632e3b2b5336eeac
icarompo <icaromesquitaponce@outlook.com>
2023-05-10 14:36:10 -0300

    Adicionando aula37 e 38

---
+++ aula38/index.js
@@ -0,0 +1,6 @@
+console.log(true && true);
+console.log(true && true && false);
+console.log(true || true);
+console.log(false || (false && true));
+console.log(!false);
+console.log(false || false && !false);
