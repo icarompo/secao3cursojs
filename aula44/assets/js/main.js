9ec9dce254c5181e578a67471b17c886ba7e5b8c
icarompo <icaromesquitaponce@outlook.com>
2023-05-11 09:21:22 -0300

    Alterando aula44

--- aula44/assets/js/main.js
+++ aula44/assets/js/main.js
@@ -8,9 +8,17 @@ function myForm(){
         const height = form.querySelector('.height');
         const result = document.querySelector('#result');
 
-        const imc = Number(weight.value)/(Number(height.value)**2);
+        const imc = Number(weight.value)/(Number(height.value)*Number(height.value));
 
         console.log(imc);
+
+        if (imc < 18.5) {
+            return `Your IMC is ${imc} and you're bellow weight` 
+        }
+        }
+
+        result.innerHTML = `${calculateImc(imc)}`;
+        if 
     }
 
     form.addEventListener('submit', eventForm);
