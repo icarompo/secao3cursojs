61f6cf40abce3236cb69c4fcdb28fa76481ba150
icarompo <icaromesquitaponce@outlook.com>
2023-05-11 10:00:40 -0300

    Alterando aula44

--- aula44/assets/js/main.js
+++ aula44/assets/js/main.js
@@ -1,18 +1,46 @@
-function myForm(){
-    const form = document.querySelector('.form');
+const form = document.querySelector(".form");
 
-    function eventForm(event){
-        event.preventDefault();
+function eventForm(event) {
+  event.preventDefault();
 
-        const weight = form.querySelector('.weight');
-        const height = form.querySelector('.height');
-        const result = document.querySelector('#result');
+  const weight = form.querySelector("#weight");
+  const height = form.querySelector("#height");
+  const result = document.querySelector("#result");
 
-        const imc = Number(weight.value)/(Number(height.value)**2);
+  let imc =
+    Number(weight.value) / (Number(height.value) * Number(height.value));
+  imc = imc.toFixed(1);
+  console.log(imc);
 
-        console.log(imc);
-    }
-
-    form.addEventListener('submit', eventForm);
+  if (imc < 18.5) {
+    result.innerHTML = `Your IMC is ${imc} and you're bellow weight`;
+    result.style.height = "30px";
+    result.style.background = "#db2c2c";
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    result.innerHTML = `Your IMC is ${imc} and your weight is normal`;
+    result.style.height = "30px";
+    result.style.background = "#11ab56";
+  } else if (imc >= 25 && imc <= 29.9) {
+    result.innerHTML = `Your IMC is ${imc} and you're on overweight`;
+    result.style.height = "30px";
+    result.style.background = "#db2c2c";
+  } else if (imc >= 30 && imc <= 34.9) {
+    result.innerHTML = `Your IMC is ${imc} and you're on the 1º degree of obesity`;
+    result.style.height = "30px";
+    result.style.background = "#db2c2c";
+  } else if (imc >= 35 && imc <= 39.9) {
+    result.innerHTML = `Your IMC is ${imc} and you're on the 2º degree of obesity`;
+    result.style.height = "30px";
+    result.style.background = "#db2c2c";
+  } else if (imc > 40) {
+    result.innerHTML = `Your IMC is ${imc} and you're on the 3º degree of obesity`;
+    result.style.height = "30px";
+    result.style.background = "#db2c2c";
+  } else {
+    result.innerHTML = `Invalid Informations`;
+    result.style.height = "30px";
+    result.style.background = "black";
+  }
 }
-myForm();
+
+form.addEventListener("submit", eventForm);
